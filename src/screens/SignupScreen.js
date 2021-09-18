37f8b2783c6948663330eb8a5d50acd26d155535
vvduth <ducthai060501@gmail.com>
2021-09-18 14:30:51 +0300

    Refactor #1

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,9 @@
 import React,{useState, useContext} from "react";
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text, Input, Button} from 'react-native-elements';
 import Spacer from "../components/Spacer";
 import { Context as AuthContext} from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SingupScreen = ({navigation}) => {
     const {state, signup} = useContext(AuthContext);
@@ -13,28 +14,20 @@ const SingupScreen = ({navigation}) => {
 
     return (
     <View style = {styles.container}>
-        <Spacer>
-            <Text h3>Sign up for Dt.Track </Text>
-        </Spacer>
-        <Input 
-            label = "Email" 
-            value={email} 
-            onChangeText= {(newEmail)=>setEmail(newEmail)}
-            autoCapitalize = "none"
-            autoCorrect = {false}    
+
+        <AuthForm 
+            headerText="Sign Up for Dt.Track"
+            errorMessage={state.errorMessage}
+            submitButtonText="Sign Up"
+            //onSubmit={({email,password}) => signup({email,password})}
+            onSubmit = {signup} //dont need to pass parm cuz it already declared upper
         />
-        <Input 
-            secureTextEntry
-            label = "Password"
-            value = {password}
-            onChangeText = {setPassword}
-            autoCapitalize = "none"
-            autoCorrect = {false}   
-        />
-        {state.errorMessage ? <Text style = {styles.errorMessage}>{state.errorMessage}</Text> : null}  
-        <Spacer>
-                <Button title = "Sign Up" onPress={()=> signup({email,password})} />
-        </Spacer>
+        
+        <TouchableOpacity onPress = {() =>   navigation.navigate('Signin')}>
+            <Spacer>
+                <Text style = {styles.link}>Already have an account? Sign in instead</Text>
+            </Spacer>
+        </TouchableOpacity>
     </View>
     );
 };
@@ -55,6 +48,9 @@ const styles = StyleSheet.create({
         color: 'red',
         marginLeft: 15,
         marginTop: 15
+    },
+    link: {
+        color: 'blue',
     }
 });
 
